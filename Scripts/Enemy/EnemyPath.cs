0425eecdea60360e5487fdfa76b4c4ed696822f8
vslayer34 <mohamd.elnagar.34@gmail.com>
2025-04-28 04:32:48 +0300

    Added enemy spawn every time interval

---
+++ Scripts/Enemy/EnemyPath.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class EnemyPath : Path3D
+{
+    [Export]
+    private PackedScene _enemyScene;
+
+    [Export]
+    private Timer _spawnTimer;
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+
+    public override void _Ready()
+    {
+        _spawnTimer.Timeout += SpawnEnemy;
+    }
+
+    public override void _ExitTree()
+    {
+        _spawnTimer.Timeout -= SpawnEnemy;
+    }
+
+
+
+    // Member Methods------------------------------------------------------------------------------
+
+    private void SpawnEnemy()
+    {
+        Enemy enemy = _enemyScene.Instantiate<Enemy>();
+        AddChild(enemy);
+    }
+}
