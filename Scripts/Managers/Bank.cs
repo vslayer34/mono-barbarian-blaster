5a9dddce33863a4dc9ccaf14e1518bb6eee15fab
vslayer34 <mohamd.elnagar.34@gmail.com>
2025-04-30 06:23:18 +0300

    Added display for the player gold amount

---
+++ Scripts/Managers/Bank.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Bank : MarginContainer
+{
+    [ExportCategory("Required Nodes")]
+
+    [Export]
+    private Label _goldAmountLabel;
+    [ExportCategory("")]
+    
+    private int _goldAmount = 150;
+    
+    
+    [Export(PropertyHint.Range, "0,999999,")]
+    public int GoldAmount
+    {
+        get => _goldAmount;
+        set
+        {
+            _goldAmount = value <= 0 ? 0 : value;
+
+            if (_goldAmountLabel != null)
+            {
+                _goldAmountLabel.Text = $"Gold: {_goldAmount}";
+            }
+        }
+    }
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+
+    public override void _Ready()
+    {
+        _goldAmountLabel.Text = $"Gold: {_goldAmount}";
+    }
+
+
+    // Member Methods------------------------------------------------------------------------------
+}
